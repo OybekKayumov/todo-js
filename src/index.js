c4dfe11bb8c90c8f7f725556e2096e3c344bbd6f
OybekKayumov <oybek.kayumov@gmail.com>
2022-01-11 15:12:09 +0500

    add HtmlWebpackPlugin, CSS-Loader, webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
@@ -6,6 +7,7 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
